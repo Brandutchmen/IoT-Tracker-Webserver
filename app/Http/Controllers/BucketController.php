6fe3ab9d773a35ad454cc24d07c360e3210b4cb4
Brandon Young <32550081+Brandutchmen@users.noreply.github.com>
2020-11-22 12:55:34 -0600

    Quick and dirty entry logic rework

--- app/Http/Controllers/BucketController.php
+++ app/Http/Controllers/BucketController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Entry;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class BucketController extends Controller
@@ -13,15 +14,32 @@ class BucketController extends Controller
         if ($request->has('macs')) {
             $macs = $request->get('macs');
             foreach($macs as $mac) {
-                $entry = new Entry();
-                $entry->mac_address = $mac;
-                $entry->bucket_id = 1;
-                $entry->save();
+                $old_entry = \App\Models\Entry::where('mac_address', $mac)->orderBy('updated_at', 'desc')->get()->first();
+                if ($old_entry != null) {
+                    $diff = \Carbon\Carbon::parse($old_entry->updated_at)->diffInMinutes(\Carbon\Carbon::now());
+                    if($diff <= env('ENTRY_SPLIT_DURATION', 15)) {
+                        $old_entry->updated_at = Carbon::now();
+                        $old_entry->save();
+                    }
+                    else {
+                        $this->createEntry($mac);
+                    }
+                }
+                else {
+                    $this->createEntry($mac);
+                }
             }
             return "Success";
         }
         else {
             return "Invalid Data";
         }
+    }
+
+    function createEntry($mac, $bucket=1) {
+        $entry = new Entry();
+        $entry->mac_address = $mac;
+        $entry->bucket_id = $bucket;
+        $entry->save();
     }
 }
